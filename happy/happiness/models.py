913d0ff855eb69aba52b3d54510bdd68148b1349
Iulia Pasov <iulia.pasov@gmail.com>
2014-10-29 14:35:18 +0000

    Added choices for privacy in messages model

--- happy/happiness/models.py
+++ happy/happiness/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Message(models.Model):
+    '''Message class
+    '''
+    PUBLIC = 'pub'
+    PRIVATE = 'prv'
+    message_choices = (
+        (PUBLIC, 'Public'),
+        (PRIVATE, 'Private'),
+    )
     message_content = models.TextField()
     pub_date = models.DateTimeField('Date')
-    privacy = models.CharField(max_length=50)
-    author = models.ForeignKey(User)
+    privacy = models.CharField(
+        max_length=50,
+        choices=message_choices,
+        default=PUBLIC)
+#    author = models.ForeignKey(User)
