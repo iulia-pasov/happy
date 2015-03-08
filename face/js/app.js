58df04d0fc871692d6bf395b0cf03b879fa50a57
Iulia Pasov <iulia.pasov@gmail.com>
2015-03-08 11:04:58 +0200

    Added preview directive and message form

--- face/js/app.js
+++ face/js/app.js
@@ -1,1 +1,26 @@
-var app = angular.module('happy', [ ]);
+(function() {
+    var app = angular.module('happy', [ ]);
+    app.controller('HappiController', function () {
+        this.test = 'iulia';
+        });
+    app.directive('messagePreview', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'preview.html'
+        };
+    });
+    app.controller('ListController', function($scope, $http) {
+        var vm = this;
+        $http.get('http://happy.local/messages/').then(function(data) {
+            vm.messages = data.data.results;
+        });
+    });
+    app.directive('listMessages', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'list.html',
+            controller: 'ListController',
+            controllerAs: 'list'
+        };
+    });
+})();
